209f9ddaf93dccbece9e6224af2e6b9ab2e6b7cd
AlejandroMigueles <alexmigueles@gmail.com>
2022-08-29 20:20:22 -0500

    Feat:se crea el primer componente y se actualizan imagenes del html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import FormSearch from './components/FormSearch'
 
 function App() {
 
   return (
     <div className="App">
       <h2>main component</h2>
+      <FormSearch />
     </div>
   )
 }
---
+++ src/components/FormSearch.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormSearch = () => {
+  return (
+    <h3>Form Search</h3>
+  )
+}
+
+export default FormSearch
