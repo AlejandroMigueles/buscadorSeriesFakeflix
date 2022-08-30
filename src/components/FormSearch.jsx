2e0a8489bc075024f0d7c71ab0d7d15b8ff41f37
AlejandroMigueles <alexmigueles@gmail.com>
2022-08-29 21:27:16 -0500

    Feat:se añaden estilos

--- src/components/FormSearch.jsx
+++ src/components/FormSearch.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { React, useState } from 'react'
 
 const FormSearch = () => {
+  const [title, setTitle] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('titulo', title)
+  }
   return (
-    <h3>Form Search</h3>
+    <div className='form-search'>
+      <h2>Buscador de series y películas</h2>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Titulo de la pelicula' onChange={e => setTitle(e.target.value)} />
+        <input type='submit' value='Search' />
+      </form>
+    </div>
   )
 }
 
